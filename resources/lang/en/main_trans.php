184acd06986e68034f0c55cb6a24f456dc5d8e12
asus <mohammadsulaiman870@gmail.com>
2021-02-12 15:29:44 +0200

    first commit

---
+++ resources/lang/en/main_trans.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'Dashboard'=>'Dashboard',
+    'Dashboard_page'=>'Dashboard_page',
+    'Main_title'=>'Main_title',
+    'Programname' => 'Programname',
+    'change_language'=>'change_language',
+    'Grades'=>'Grades',
+    'Grades_list'=>'Grades_list',
+    'classes'=>'classes',
+    'List_classes'=>'List_classes',
+    'sections'=>'sections',
+    'students'=>'students',
+    'Teachers'=>'Teachers',
+    'Parents'=>'Parents',
+    'Accounts'=>'Accounts',
+    'Attendance'=>'Attendance',
+    'Exams'=>'Exams',
+    'library'=>'library',
+    'Onlineclasses'=>'Onlineclasses',
+    'Settings'=>'Settings',
+    'Users'=>'Users',
+    'Copyright' => 'Copyright ',
+    'Name_Programer' => 'Mohammad Sulaiman'
+
+
+];
