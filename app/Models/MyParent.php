43db72fc0b56b6155106d40b05e1173bd0aa2658
asus <mohammadsulaiman870@gmail.com>
2021-03-05 15:40:45 +0200

    Real-Time Validation Livewire and insert in data base

--- app/Models/MyParent.php
+++ app/Models/MyParent.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class MyParent extends Model
 {
     use HasFactory;
+    use HasTranslations;
+
+    protected $guarded=[];
+    protected $table = 'my_parents';
+    public $translatable = ['Name_Father','Job_Father','Name_Mother','Job_Mother'];
+
 }
